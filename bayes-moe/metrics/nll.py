00b4a0581b412e41fe0ef152fe0be58ea65e494f
Daoyuan GUO <dguo294@connect.hkust-gz.edu.cn>
2025-09-04 09:20:35 +0000

    Revert "Week 1 startup 3"
    This reverts commit 3a1c3ac136b7107563f0e8aae2d0bdf1feda9aa4.

--- bayes-moe/metrics/nll.py
+++
@@ -1,6 +0,0 @@
-import torch
-import torch.nn.functional as F
-
-@torch.no_grad()
-def negative_log_likelihood(logits: torch.Tensor, targets: torch.Tensor) -> float:
-    return F.cross_entropy(logits, targets, reduction="mean").item()
